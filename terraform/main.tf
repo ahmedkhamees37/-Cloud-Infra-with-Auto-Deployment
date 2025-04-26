59d8806f3dd247a543a8f42a2daf15d499244414
Ahmed Khamis Hassan <ahmedkhamees37@gmail.com>
2025-04-26 15:22:24 +0300

    5

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration ooptions
+  # Cconfiguration ooptions
   region = "us-east-1"
   #access_key              = var.aws_access_key
   #secret_access_key       = var.aws_secret_key
